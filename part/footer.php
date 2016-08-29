366930ebeca385f37d4253de5b237a0dc0614579
maulayyacyber <ridaulmaulayya@gmail.com>
2016-08-29 16:10:43 +0700

    perubahan style mode mobile

--- part/footer.php
+++ part/footer.php
@@ -2,8 +2,8 @@
         <p>&copy; 2016 Company, Inc.</p>
       </footer>
     </div>
-    <script src="<?php echo $config['base_url'] ?>js/jquery.min.js"></script>
-    <script src="<?php echo $config['base_url'] ?>js/bootstrap.min.js"></script>
+    <script src="<?php echo $config['base_url'] ?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo $config['base_url'] ?>assets/js/bootstrap.min.js"></script>
     <script src="<?php echo $config['base_url'] ?>assets/js/ie10-viewport-bug-workaround.js"></script>
   </body>
 </html>
