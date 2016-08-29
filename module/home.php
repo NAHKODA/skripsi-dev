2ef1aa7beb01e05b13d3d4ad013b2c60a8bf00e7
maulayyacyber <ridaulmaulayya@gmail.com>
2016-08-29 16:25:13 +0700

    style jumbotron material with background image

--- module/home.php
+++ module/home.php
@@ -1,8 +1,8 @@
     <!-- Main jumbotron for a primary marketing message or call to action -->
-    <div class="jumbotron">
-      <div class="container">
-        <h2>Universitas KH. A. Wahab Hasbullah</h2>
-        <p>Untuk mengajukan sebuah judul skripsi mahasiswa diharuskan untuk masuk panel system dan mulai mengajukan sebuah judul skripsi dan minimal mengajukan 2 buah judul skripsi.</p>
+    <div class="jumbotron" style="background-image: url('https://public-assets.envato-static.com/assets/common/bg-page-section--light-dbffac9426e51e0fdcaaad8452d7561b.png'); position:relative;background-color:#1abc9c">
+      <div class="container" style="color:#fff">
+        <h2 style="font-family: 'Lato'; font-weight:bold;font-size:35px">Universitas KH. A. Wahab Hasbullah</h2>
+        <p style="font-family: 'Lato'; font-weight:bold;font-size:20px">Untuk mengajukan sebuah judul skripsi mahasiswa diharuskan untuk masuk panel system dan mulai mengajukan sebuah judul skripsi dan minimal mengajukan 2 buah judul skripsi.</p>
         <p><a class="btn btn-default btn-lg" href="mahasiswa" role="button"><i class="fa fa-lock"></i> Masuk Mahasiswa <i class="fa fa-long-arrow-right"></i></a></p>
       </div>
     </div>
@@ -10,20 +10,23 @@
     <div class="container">
       <!-- Example row of columns -->
       <div class="row">
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-        </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
-       </div>
-        <div class="col-md-4">
-          <h2>Heading</h2>
-          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        <div class="col-md-12">
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+          </div>
+          <div class="panel panel-default">
+            <div class="panel-heading">
+              <h3 class="panel-title">Panel title</h3>
+            </div>
+            <div class="panel-body">
+              Panel content
+            </div>
+          </div>          
         </div>
       </div>
 
